dfdaaf9ae8a2e7e070b2da27c4e1045d9a87b850
Tor Vallin <c18tvn@cs.umu.se>
2023-10-08 18:54:57 +0200

    Add basic player movement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use bevy::{
     prelude::{
-        App, AssetServer, Camera3dBundle, Commands, Component, Entity, Mesh, Plugin, PointLight,
-        PointLightBundle, Query, Res, ResMut, Resource, Startup, Transform, Update, Vec3, With,
+        App, AssetServer, Camera3dBundle, Commands, Component, Entity, Input, KeyCode, Mesh,
+        Plugin, PointLight, PointLightBundle, Quat, Query, Res, ResMut, Resource, Startup,
+        Transform, Update, Vec3, With,
     },
     scene::SceneBundle,
     time::{Time, Timer, TimerMode},
@@ -12,7 +13,6 @@ use bevy::{
 struct Player {
     entity: Option<Entity>,
     lives: u32,
-    position: Vec3,
 }
 
 #[derive(Resource, Default)]
@@ -23,12 +23,17 @@ struct GameState {
 
 fn setup_cameras(mut commands: Commands, mut game: ResMut<GameState>) {
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 6.0, 2.0).looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+        transform: Transform::from_xyz(0.0, 6.0, 2.0)
+            .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
         ..Default::default()
     });
 }
 
-fn setup_game_state(mut commands: Commands, asset_server: Res<AssetServer>, mut game: ResMut<GameState>) {
+fn setup_game_state(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut game: ResMut<GameState>,
+) {
     game.score = 0;
     game.player.lives = 3;
 
@@ -37,6 +42,7 @@ fn setup_game_state(mut commands: Commands, asset_server: Res<AssetServer>, mut 
             .spawn(SceneBundle {
                 transform: Transform {
                     translation: Vec3::new(0.0, 0.0, 0.0),
+                    rotation: Quat::from_rotation_y(90.0_f32.to_radians()),
                     ..Default::default()
                 },
                 scene: asset_server.load("Spaceship4/model.obj"),
@@ -51,15 +57,43 @@ fn setup_game_state(mut commands: Commands, asset_server: Res<AssetServer>, mut 
             shadows_enabled: true,
             ..Default::default()
         },
+        transform: Transform {
+            translation: Vec3::new(3.0, 2.0, 0.0),
+            ..Default::default()
+        },
         ..Default::default()
     });
 }
 
 fn player_controls(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut game: ResMut<GameState>,
+    _: Commands,
+    input: Res<Input<KeyCode>>,
+    game: ResMut<GameState>,
+    mut transforms: Query<&mut Transform>,
+    time: Res<Time>,
 ) {
+    let player = game.player.entity.unwrap();
+
+    let mut transform = transforms.get_mut(player).unwrap();
+    let mut translation = transform.translation;
+
+    // Move left and right with A/D
+    if input.pressed(KeyCode::A) {
+        translation.x -= 1.0 * time.delta_seconds();
+        *transform = Transform {
+            translation,
+            rotation: transform.rotation, 
+            ..Default::default()
+        }
+    }
+    if input.pressed(KeyCode::D) {
+        translation.x += 1.0 * time.delta_seconds();
+        *transform = Transform {
+            translation,
+            rotation: transform.rotation,
+            ..Default::default()
+        }
+    }
 }
 
 fn main() {
