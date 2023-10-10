4fc3355082cda77b71ab0aa40fc902771e43b704
Tor Vallin <c18tvn@cs.umu.se>
2023-10-10 20:12:17 +0200

    Add player bullet shooting, with cooldown

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,45 @@
+use std::ops::Add;
+
 use bevy::{
     prelude::{
-        App, AssetServer, Camera3dBundle, Commands, Component, Entity, Input, KeyCode, Mesh,
-        Plugin, PointLight, PointLightBundle, Quat, Query, Res, ResMut, Resource, Startup,
-        Transform, Update, Vec3, With,
+        App, AssetServer, Camera3dBundle, Commands, Component, Entity, Input, KeyCode, PointLight,
+        PointLightBundle, Quat, Query, Res, ResMut, Resource, Startup, Transform, Update, Vec3,
+        With,
     },
     scene::SceneBundle,
-    time::{Time, Timer, TimerMode},
+    time::Time,
     transform::TransformBundle,
     DefaultPlugins,
 };
 use bevy_rapier3d::{
     prelude::{
-        ActiveEvents, Ccd, Collider, NoUserData, RapierContext, RapierPhysicsPlugin, RigidBody,
-        Sensor, Sleeping, GravityScale,
+        ActiveEvents, Collider, GravityScale, NoUserData, RapierContext, RapierPhysicsPlugin,
+        RigidBody, Sensor,
     },
     render::RapierDebugRenderPlugin,
 };
 
-#[derive(Default)]
+#[derive(Component, Default)]
 struct Player {
-    entity: Option<Entity>,
     lives: u32,
+    bullet_cooldown: f32,
+    bullet_cooldown_timer: f32,
 }
 
 #[derive(Resource, Default)]
 struct GameState {
-    player: Player,
+    player: Option<Entity>,
     score: u32,
 }
 
 #[derive(Component)]
 struct EnemyBullet {}
+
+#[derive(Component)]
+struct Bullet {
+    up_direction: bool,
+    velocity: f32,
+}
 
 fn setup_cameras(mut commands: Commands, _: ResMut<GameState>) {
     commands.spawn(Camera3dBundle {
@@ -45,9 +54,8 @@ fn setup_game_state(
     mut game: ResMut<GameState>,
 ) {
     game.score = 0;
-    game.player.lives = 3;
 
-    game.player.entity = Some(
+    game.player = Some(
         commands
             .spawn(SceneBundle {
                 transform: Transform {
@@ -62,6 +70,11 @@ fn setup_game_state(
             .insert(GravityScale(0.0))
             .insert(Collider::cylinder(0.25, 0.3))
             .insert(ActiveEvents::COLLISION_EVENTS)
+            .insert(Player {
+                lives: 0,
+                bullet_cooldown: 0.0,
+                bullet_cooldown_timer: 0.25,
+            })
             .id(),
     );
 
@@ -88,34 +101,57 @@ fn setup_game_state(
 }
 
 fn player_controls(
-    _: Commands,
+    mut commands: Commands,
     input: Res<Input<KeyCode>>,
     game: ResMut<GameState>,
-    mut transforms: Query<&mut Transform>,
+    mut player_query: Query<(&mut Transform, &mut Player)>,
     time: Res<Time>,
 ) {
-    let player = game.player.entity.unwrap();
+    let player_entity = game.player.unwrap();
 
-    let mut transform = transforms.get_mut(player).unwrap();
-    let mut translation = transform.translation;
+    let mut player = player_query.get_mut(player_entity).unwrap();
+    let mut translation = player.0.translation;
 
     let move_speed = 3.0;
     // Move left and right with A/D
     if input.pressed(KeyCode::A) {
         translation.x -= move_speed * time.delta_seconds();
-        *transform = Transform {
+        *player.0 = Transform {
             translation,
-            rotation: transform.rotation,
+            rotation: player.0.rotation,
             ..Default::default()
         }
     }
     if input.pressed(KeyCode::D) {
         translation.x += move_speed * time.delta_seconds();
-        *transform = Transform {
+        *player.0 = Transform {
             translation,
-            rotation: transform.rotation,
+            rotation: player.0.rotation,
             ..Default::default()
         }
+    }
+
+    let can_shoot = if player.1.bullet_cooldown <= 0.0 {
+        true
+    } else {
+        player.1.bullet_cooldown -= time.delta_seconds();
+        false
+    };
+
+    if can_shoot && input.pressed(KeyCode::Space) {
+        player.1.bullet_cooldown = player.1.bullet_cooldown_timer;
+        commands
+            .spawn(Collider::cuboid(0.05, 0.05, 0.1))
+            .insert(RigidBody::Fixed)
+            .insert(Sensor)
+            .insert(Bullet {
+                up_direction: true,
+                velocity: 7.5,
+            })
+            .insert(ActiveEvents::COLLISION_EVENTS)
+            .insert(TransformBundle::from(Transform::from_translation(
+                translation.add(Vec3::new(0.0, 0.0, -0.5)),
+            )));
     }
 }
 
@@ -126,7 +162,7 @@ fn main() {
         .add_plugins(RapierDebugRenderPlugin::default())
         .init_resource::<GameState>()
         .add_systems(Startup, (setup_cameras, setup_game_state))
-        .add_systems(Update, player_controls)
+        .add_systems(Update, (player_controls, bullet_controls))
         .add_systems(Update, check_intersections)
         .run();
 }
@@ -137,7 +173,7 @@ fn check_intersections(
     enemy_bullets: Query<Entity, (With<Collider>, With<EnemyBullet>)>,
 ) {
     for bullet in &enemy_bullets {
-        let player = game.player.entity.unwrap();
+        let player = game.player.unwrap();
 
         // Checks for intersections between the player and the enemy bullet
         if rapier_context.intersection_pair(player, bullet) == Some(true) {
@@ -146,5 +182,17 @@ fn check_intersections(
                 player, bullet
             );
         }
+    }
+}
+
+fn bullet_controls(
+    _: ResMut<GameState>,
+    mut bullets: Query<(&mut Transform, &Bullet), (With<Collider>, With<Bullet>)>,
+    time: Res<Time>,
+) {
+    let delta_time = time.delta_seconds();
+    for (mut transform, bullet) in bullets.iter_mut() {
+        let direction = if bullet.up_direction { -1.0 } else { 1.0 };
+        transform.translation.z += direction * bullet.velocity * delta_time;
     }
 }
