84f090054c58939375f4db013afe34563f3031c3
Tor Vallin <c18tvn@cs.umu.se>
2023-10-15 13:45:31 +0200

    Implement bullet damage

--- src/combat.rs
+++ src/combat.rs
@@ -1,18 +1,14 @@
 use bevy::prelude::Component;
 
 #[derive(Component)]
-pub struct Health {
+pub struct Damageable {
     pub health: u32,
+    pub is_player: bool,
 }
 
 #[derive(Component)]
-pub struct EnemyBullet {}
-
-#[derive(Component)]
-pub struct PlayerBullet {}
-
-#[derive(Component)]
 pub struct Bullet {
+    pub is_player_bullet: bool,
     pub up_direction: bool,
     pub velocity: f32,
     pub damage: u32,
